d60b8e485e08d84a2e995702f3fb8bc522488be9
atlex00 <atlex00@gmail.com>
2022-01-19 20:07:10 +0100

    Done video part1

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 I followed this YouTube video for learning [his](https://github.com/rexim) thinking.
 
-https://www.youtube.com/watch?v=tR6p7ZC7RaU
+- Part 1: https://www.youtube.com/watch?v=tR6p7ZC7RaU
+- Part 2: https://www.youtube.com/watch?v=Iveh2W3roJk
+- Part 3: https://www.youtube.com/watch?v=Uj0CrPM65Rc
 
 ## Progresses
 
-Jan. 16. 2021: 0:00:00 ~ 1:45:53
+- Jan. 16. 2021: 0:00:00 ~ 1:45:53 Part1
+- Jan. 18. 2021: Rest of the video (Part1 Done)
 
 ## Original source
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use ncurses::*;
-use std::cmp::*;
+use std::fs::File;
+use std::io::{self, Write, BufRead};
+use std::env;
+use std::process;
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
@@ -26,7 +29,7 @@ impl Ui {
     }
 
     fn list_element(&mut self, label: &str, id: Id) {
-        let id_curr = self.list_curr.expect("Note allowed to create list elements outside of lists"); 
+        let id_curr = self.list_curr.expect("Not allowed to create list elements outside of lists"); 
         self.label(label, {
             if id_curr == id {
                 HIGHLIGHT_PAIR
@@ -52,7 +55,110 @@ impl Ui {
     }
 }
 
+#[derive(Debug)]
+enum Status {
+    Todo,
+    Done
+}
+
+impl Status{
+    fn toggle(&self) -> Self {
+        match self {
+            Status::Todo => Status::Done,
+            Status::Done => Status::Todo,
+        }
+    }
+}
+
+fn parse_item(line: &str) -> Option<(Status, &str)> {
+    let todo_prefix = "TODO: ";
+    let done_prefix = "DONE: ";
+
+    if line.starts_with(todo_prefix) {
+        return Some((Status::Todo, &line[todo_prefix.len()..]))
+    }
+
+    if line.starts_with(done_prefix) {
+        return Some((Status::Done, &line[done_prefix.len()..]))
+    }
+
+    return None;
+}
+
+fn list_up(list_curr: &mut usize){
+    if *list_curr > 0 {
+        *list_curr -= 1;
+    }
+
+}
+
+fn list_down(list: &Vec<String>, list_curr: &mut usize){
+    if *list_curr + 1 < list.len() {
+        *list_curr += 1;
+    }
+}
+
+fn list_transfer(list_dest: &mut Vec<String>, list_src: &mut Vec<String>, list_src_curr: &mut usize) {
+    if *list_src_curr < list_src.len() {
+        list_dest.push(list_src.remove(*list_src_curr));
+        if *list_src_curr >= list_src.len() && list_src.len() >0 {
+            *list_src_curr = list_src.len() -1;
+        }
+    }
+}
+
+fn load_state(todos: &mut Vec<String>, dones: &mut Vec<String>, file_path: &str) {
+    let file = File::open(file_path).unwrap();
+    for (index, line) in io::BufReader::new(file).lines().enumerate() {
+        match parse_item(&line.unwrap()) {
+            Some((Status::Todo, title)) => todos.push(title.to_string()),
+            Some((Status::Done, title)) => dones.push(title.to_string()),
+            None => {
+                eprintln!("{}:{}: ERROR: ill-formed item line", file_path, index +1 );
+                process::exit(1);
+            }
+        }
+    }
+}
+
+fn save_state(todos: &Vec<String>, dones: &Vec<String>, file_path: &str) {
+    let mut file = File::create(file_path).unwrap();
+    for todo in todos.iter() {
+         writeln!(file, "TODO: {}", todo).unwrap();
+    }
+    for done in dones.iter() {
+        writeln!(file, "DONE: {}", done).unwrap();
+    }
+}
+
+// TODO: persiste the state of the application
+// TODO: add new items to TODO
+// TODO: delete items
+// TODO: edit items
+// TODO: keep track of date when the item was DONE
+// TODO: undo system
+// TODO: save the state on SIGINT
+
 fn main() {
+    let mut args = env::args();
+    args.next().unwrap();
+
+    let file_path = match args.next() {
+        Some(file_path) => file_path,
+        None => {
+            eprintln!("Usage: todo-rs <file-path>");
+            eprintln!("ERROR: file path is not provided");
+            process::exit(1)
+        }
+    };
+
+    let mut todos = Vec::<String>::new();
+    let mut todo_curr: usize = 0;
+    let mut dones = Vec::<String>::new();
+    let mut done_curr: usize = 0;
+
+    load_state(&mut todos, &mut dones, &file_path);
+
     initscr();
     noecho();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
@@ -62,39 +168,36 @@ fn main() {
     init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
 
     let mut quit = false;
-    let mut todos: Vec::<String> = vec![
-        "Wrtie the todo app".to_string(),
-        "Buy a bread".to_string(),
-        "Make a cup of tea".to_string(),
-    ];
-    let mut todo_curr: usize = 0;
-    let mut dones: Vec::<String> = vec![
-        "Start the stream".to_string(),
-        "Have a breakfast".to_string(),
-        "Make a cup of tea".to_string(),
-    ];
-    let mut done_curr: usize = 0;
+    let mut status = Status::Todo;
 
     let mut ui = Ui::default();
     while !quit {
         erase();
         ui.begin(0, 0);
         {
-            ui.label("TODO:", REGULAR_PAIR);
-            ui.begin_list(todo_curr);
-            for (index, todo) in todos.iter().enumerate() {
-                ui.list_element(&format!("- [ ] {}",todo), index);
-            }
-            ui.end_list();
+            match status {
+                Status::Todo => {
+                    ui.label("[TODO] DONE ", REGULAR_PAIR);
+                    ui.label("------------", REGULAR_PAIR);
+                    ui.begin_list(todo_curr);
+                    for (index, todo) in todos.iter().enumerate() {
+                        ui.list_element(&format!("- [ ] {}",todo), index);
+                    }
+                    ui.end_list();
+                    },
+                Status::Done => {
+                    ui.label(" TODO [DONE]", REGULAR_PAIR);
+                    ui.label("------------", REGULAR_PAIR);
+                    ui.begin_list(done_curr);
+                    for (index,done) in dones.iter().enumerate() {
+                        ui.list_element(&format!("- [x] {}",done), index);
+                    }
+                    ui.end_list();
+                }
 
-            ui.label("--------------------", REGULAR_PAIR);
-
-            ui.label("DONE:", REGULAR_PAIR);
-            ui.begin_list(0);
-            for (index,done) in dones.iter().enumerate() {
-                ui.list_element(&format!("- [x] {}",done), index+1);
             }
-            ui.end_list();
+
+
         }
         ui.end();
 
@@ -103,20 +206,34 @@ fn main() {
         let key = getch();
         match key as u8 as char {
             'q' => quit = true,
-            'w' => if todo_curr > 0  {
-                    todo_curr -= 1;
-                },
-            's' => if todo_curr + 1 < todos.len() {
-                    todo_curr += 1;
-            },
-            '\n' => {
-                if todo_curr < todos.len() {
-                    dones.push(todos.remove(todo_curr));
+            'w' => {
+                match status {
+                    Status::Todo => list_up(&mut todo_curr),
+                    Status::Done => list_up(&mut done_curr),
                 }
             },
-            _   => {}
+            's' => {
+                match status {
+                    Status::Todo => list_down(&todos, &mut todo_curr),
+                    Status::Done => list_down(&dones, &mut done_curr),
+                }
+            },
+            '\n' => {
+                match status {
+                    Status::Todo => list_transfer(&mut dones, &mut todos, &mut todo_curr),
+                    Status::Done => list_transfer(&mut todos, &mut dones, &mut done_curr)
+                }
+            },
+            '\t' => {
+                status = status.toggle();
+            },
+            _   => {
+                    // todos.push(format!("{}", key));
+            }
         }
     }
+
+    save_state(&todos, &dones, &file_path);
 
     endwin();
 }
